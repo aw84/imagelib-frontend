7c8bf7270ab5500cb55d6dbeffd100357b30c5c6
aw84 <silencewalker@gmail.com>
2022-05-01 13:30:28 +0200

    overlay thymeleaf fragment for image popup

--- src/main/java/pl/aw84/imagelib/frontend/controller/IndexController.java
+++ src/main/java/pl/aw84/imagelib/frontend/controller/IndexController.java
@@ -1,5 +1,7 @@
 package pl.aw84.imagelib.frontend.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -37,5 +39,26 @@ public class IndexController {
         model.addAttribute("totalPages", pir.getTotalPages());
         model.addAttribute("currentPage", pir.getNumber());
         return "index-b";
+    }
+
+    @GetMapping(value = "/fragments/imageOverlay")
+    public String getImageOverlayFragment(Model model, @RequestParam(defaultValue = "0") UUID p) {
+
+        String storageRelativePath = webClientBuilder.build().get()
+                .uri("http://api/storage/{p}", p)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        model.addAttribute("id", p);
+        model.addAttribute("imageSrc", imageSourceHost + "/" + storageRelativePath);
+        return "fragments/imageOverlay.html :: imageOverlay";
+    }
+
+    @GetMapping(value = "/fragments/header")
+    public String getHeaderFragment(Model model, @RequestParam(defaultValue = "0") Integer p) {
+
+        model.addAttribute("id", p);
+        return "fragments/header.html :: header";
     }
 }
--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,7 +1,14 @@
 function imageBigView(img) {
-    $('#imageOverlay').html("<img id='image' src='" + img.src + "'/>");
+    $.ajax({
+        url: "/fragments/imageOverlay?p=" + img.id,
+        success: function (result) {
+            console.log(result);
+            $('#imageOverlay').html(result);
+        }
+    });
     $('#imageOverlay').click(function () { $(this).dialog("close"); });
     $('#imageOverlay').dialog({
+        position: { my: "top", at: "top", of: window },
         dialogClass: "no-close",
         modal: true,
         minWidth: 700,
